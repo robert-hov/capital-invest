5d27d86ad2cbe4c40cee659cd75f0c72c22c6864
robert-hov <roberthvn88@gmail.com>
2023-02-09 20:18:13 +0400

    mobile plus tablet not finished

--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -1,64 +1,64 @@
 import React from "react";
-import { Parallax } from "react-parallax";
+import {Parallax} from "react-parallax";
 import Button from "../../components/Button";
-import {useTranslation, withI18n} from 'react-i18next'
+import {withI18n} from 'react-i18next'
 import "./style.scss";
 
 const Hero = ({
-  bgImg,
-  other = false,
-  inner = false,
-  withButton = false,
-  text,
-  btnText,
-  logo = false,
-  t
-}) => {
-  const title = t('hero').split("");
-  return (
-    <>
-      <div
-        className={`hero${other ? " hero--other" : ""}${
-          inner ? " hero--inner" : ""
-        }`}
-      >
-        <div className="hero__img">
-        <Parallax strength={300}
-         renderLayer={x => (
-          <div
-              style={{
-                  width: 'auto',
-                  height: '100%',
-              }}
-          />
-      )}
-      bgImageStyle={{minHeight: '100%', width: 'auto'}}
-        bgImage={bgImg}>
-          
-          {/* <img src={bgImg} alt="capital invest" /> */}
-        </Parallax>
-        </div>
-        <div className="hero__content">
-          {logo ? (
-            <div className="hero__logo">
-              <div className="img-container">
-                <img src={logo} alt="logo" />
-              </div>
+                  bgImg,
+                  other = false,
+                  inner = false,
+                  withButton = false,
+                  text,
+                  btnText,
+                  logo = false,
+                  t
+              }) => {
+    const title = t('hero').split("");
+    return (
+        <>
+            <div
+                className={`hero${other ? " hero--other" : ""}${
+                    inner ? " hero--inner" : ""
+                }`}
+            >
+                <div className="hero__img">
+                    <Parallax strength={300}
+                              renderLayer={x => (
+                                  <div
+                                      style={{
+                                          width: 'auto',
+                                          height: '100%',
+                                      }}
+                                  />
+                              )}
+                              bgImageStyle={{minHeight: '100%', width: 'auto'}}
+                              bgImage={bgImg}>
+
+                        {/* <img src={bgImg} alt="capital invest" /> */}
+                    </Parallax>
+                </div>
+                <div className="hero__content">
+                    {logo ? (
+                        <div className="hero__logo">
+                            <div className="img-container">
+                                <img src={logo} alt="logo"/>
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <h1 className="hero__title">
+                                {title.map((el, i) => {
+                                    return <span key={i}>{el}</span>;
+                                })}
+                            </h1>
+                            {withButton ? <Button text={btnText} link="/"/> : null}
+                        </>
+                    )}
+                </div>
             </div>
-          ) : (
-            <>
-              <h1 className="hero__title">
-                {title.map((el, i) => {
-                  return <span key={i}>{el}</span>;
-                })}
-              </h1>
-              {withButton ? <Button text={btnText} link="/" /> : null}
-            </>
-          )}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default withI18n()(Hero);
